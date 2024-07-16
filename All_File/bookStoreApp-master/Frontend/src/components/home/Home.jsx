30588d8d7fbde85f762fed9e51b1fd713a3c2a1d
Santu Jana <115920400+akku27-cse@users.noreply.github.com>
2024-07-16 23:22:27 +0000

    All File

---
+++ All_File/bookStoreApp-master/Frontend/src/components/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Banner from "../Banner/Banner";
+import Freebook from "../Freebook/Freebook";
+import Footer from "../Footer/Footer";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Freebook />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
